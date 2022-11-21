fec3d9612f5b57a6c260cfca589f0a770adafb87
Militskiy <alexei.militski@yandex.ru>
2022-11-21 14:07:08 +0400

    Feature: added booking entity and repository

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,19 +1,66 @@
 package ru.practicum.shareit.booking.model;
 
+import com.google.common.base.Objects;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
-import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
 
-@Data
-@Builder
+@Entity
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "bookings")
 public class Booking {
+    @Id
+    @Column(name = "booking_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private LocalDateTime start;
-    private LocalDateTime end;
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(name = "start_time")
+    private Date start;
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(name = "end_time")
+    private Date end;
+    @ManyToOne
+    @JoinColumn(name = "item_id")
     private Item item;
+    @ManyToOne
+    @JoinColumn(name = "booker_id")
     private User booker;
+    @Enumerated(EnumType.STRING)
     private Status status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+        return getId() == booking.getId() && Objects.equal(getItem(), booking.getItem()) && Objects.equal(getBooker(), booking.getBooker());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getItem(), getBooker());
+    }
 }
